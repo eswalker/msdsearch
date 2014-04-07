0a611f4dfc20712210bdde3aa4871c5f2c6a8e52
stevefsswanson <swanson.stev@gmail.com>
2014-04-07 13:59:25 -0400

    server now displays trackIDs and weights of found tracks.

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import SocketServer
 import SimpleHTTPServer
 import sys
 import urllib
+import postings 
 
 query_split = "?q="
 
@@ -10,8 +11,13 @@ class Reply(SimpleHTTPServer.SimpleHTTPRequestHandler):
 		# query arrives in self.path; return anything, e.g.,
 		terms = self.path.split('/')
 		decoded_path = urllib.unquote(self.path.encode('utf8'))	
-		query = decoded_path[decoded_path.index(query_split) + len(query_split):]			
-		self.wfile.write("%s" % query.encode('utf-8'))
+		query = decoded_path[decoded_path.index(query_split) + len(query_split):]		
+		query = query.split(',')
+
+		mainlist = postings.getlist(query[0])
+		for i in xrange(1, len(query)):
+			mainlist = postings.intersectlists(mainlist, postings.getlist(query[i]))
+		self.wfile.write("%s" % str(postings.sortedtracks(mainlist)).encode('utf-8'))
 
 def main():	
 	port = 8080
