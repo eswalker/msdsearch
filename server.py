c40647d456b63d9a817bc80b3b018873197d3af2
= <eswalker@princeton.edu>
2014-04-07 13:52:18 -0400

    handle plus sign in url encoding

--- server.py
+++ server.py
@@ -8,8 +8,8 @@ query_split = "?q="
 class Reply(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	def do_GET(self):
 		# query arrives in self.path; return anything, e.g.,
-		terms = self.path.split('/')
-		decoded_path = urllib.unquote(self.path.encode('utf8'))	
+		path = self.path.encode('utf-8').replace("+", "%20")
+		decoded_path = urllib.unquote(path.encode('utf8'))	
 		query = decoded_path[decoded_path.index(query_split) + len(query_split):]			
 		self.wfile.write("%s" % query.encode('utf-8'))
 
